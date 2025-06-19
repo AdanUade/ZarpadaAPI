fce00f8d6c95b22eca773e8ed84f844c684d88a4
AdanUade <adarodriguez@uade.edu.ar>
2025-06-19 00:19:34 -0300

    a

--- backend/db/neo4j.py
+++ backend/db/neo4j.py
@@ -3,6 +3,6 @@ from neo4j import GraphDatabase
 
 NEO4J_URI = os.environ["NEO4J_URI"]
 NEO4J_USER = os.environ["NEO4J_USER"]
-NEO4J_PASS = os.environ["NEO4J_PASSWORD"]
+NEO4J_PASS = os.environ["NEO4J_PASS"]
 
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASS))
