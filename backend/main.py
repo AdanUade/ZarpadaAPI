37663351df2fdac7c636bf853e0c478353cd6ffa
AdanUade <adarodriguez@uade.edu.ar>
2025-06-19 01:56:53 -0300

    agregue imagen

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,14 @@ from backend.db.mongo import db
 from backend.db.neo4j import driver
 from backend.routers.users import router as user_router
 from backend.routers.prendas import router as prendas_router
+from backend.routers.imagen import router as imagen_router
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/api", tags=["usuarios"])
 app.include_router(prendas_router, prefix="/api", tags=["prendas"])
+app.include_router(imagen_router, prefix="/api", tags=["imagen"])
 
 @app.get("/mongo-test")
 def mongo_test():
